332849bd5115437286268f3baa2542207638d14c
Vinicius Isumi <pancake.tf2@gmail.com>
2024-08-14 20:07:15 -0300

    initial commtit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
